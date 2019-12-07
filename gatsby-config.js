63461e26e700a982fab2a6842e61b50e3c6351ef
slyvon <hi@slyvon.com>
2019-12-06 17:56:47 -0800

    asdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,12 @@ module.exports = {
 		`gatsby-plugin-catch-links`,
 		'gatsby-plugin-theme-ui',
 		{
+			resolve: "gatsby-plugin-netlify-cache",
+			options: {
+				cachePublic: false
+			}
+		},
+		{
 			resolve: `gatsby-plugin-emotion`,
 			options: {
 				// Accepts all options defined by `babel-plugin-emotion` plugin.
