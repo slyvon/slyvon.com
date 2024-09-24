1cf1fe774042cfc451e23c29732fb395850f3987
slyvon <55820893+slyvon@users.noreply.github.com>
2024-09-24 15:07:06 -0500

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { targetBlank } from "@targetBlank";
+import { targetBlank } from "./src/targetBlank";
 
 export default defineConfig({
-  site: "https://slyvon.netlify.dev",
+  site: "https://slyvon.netlify.app",
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     rehypePlugins: [[targetBlank, { domain: 'slyvon.netlify.app' }]],
