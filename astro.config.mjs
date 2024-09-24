c76ed5dee8572008fd77c9d010312c5fbedc80eb
slyvon <55820893+slyvon@users.noreply.github.com>
2024-09-24 14:59:24 -0500

    created plugin for markdown external URLs
    1. Plugin --> targetBlank.ts
    2. Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { targetBlank } from "@targetBlank";
 
 export default defineConfig({
-  site: "https://slyvon.netlify.app",
+  site: "https://slyvon.netlify.dev",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    rehypePlugins: [[targetBlank, { domain: 'slyvon.netlify.app' }]],
+  },
 });
