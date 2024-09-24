321e2af428c0d8852f96f7247e67a28ecfc30ab8
slyvon <55820893+slyvon@users.noreply.github.com>
2024-09-24 10:37:40 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://slyvon-com.pages.dev",
   integrations: [mdx(), sitemap(), tailwind()],
 });
